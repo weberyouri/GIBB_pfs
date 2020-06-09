3b52f84a604f731c195a75766e09227c15891856
Youri Weber <weber.youri@icloud.com>
2020-06-09 15:47:14 +0200

    V1

--- src/app/fight/fight.component.ts
+++ src/app/fight/fight.component.ts
@@ -12,12 +12,17 @@ export class FightComponent implements OnInit {
 
   currentMessage = 'Wähle eine Aktion!';
 
+  selectingAfterDefeat = false;
+
   playerTeam: Pokemon[];
 
   opponentTeam: Pokemon[];
 
   currentPlayerPokemon: Pokemon;
   currentOpponentPokemon: Pokemon;
+
+  currentPlayerPokemonDead = false;
+  currentOpponentPokemonDead = false;
 
   playerTurn = true;
 
@@ -37,9 +42,33 @@ export class FightComponent implements OnInit {
   }
 
   async fight(attack?: Move) {
+    this.selectingAfterDefeat = false;
+    this.menuPokemon = false;
+    this.menuFight = false;
+    this.currentPlayerPokemonDead = false;
+    this.currentOpponentPokemonDead = false;
     this.playerTurn = false;
     await this.delay(2000);
     await this.executeAttacks(attack);
+
+
+    if(this.currentOpponentPokemonDead) {
+      const index = this.opponentTeam.indexOf(this.currentOpponentPokemon);
+      this.opponentTeam.splice(index, 1);
+      await this.delay(2000);
+      const rndm = Math.floor(Math.random() * this.opponentTeam.length);
+      this.currentOpponentPokemon = this.opponentTeam[rndm];
+      this.currentMessage = 'Gegner setzt ' + this.currentOpponentPokemon.name + ' ein!';
+    }
+
+    if(this.currentPlayerPokemonDead) {
+      const index = this.playerTeam.indexOf(this.currentPlayerPokemon);
+      this.playerTeam.splice(index, 1);
+      await this.delay(2000);
+      await this.selectNewPokemon();
+    }
+
+    this.playerTurn = true;
   }
 
   switchPokemon(pkmn: Pokemon) {
@@ -50,6 +79,16 @@ export class FightComponent implements OnInit {
 
   getStat(name: string, pokemon: Pokemon) {
     return pokemon.stats.find(s => s.stat.name === name);
+  }
+
+  async changePokemon(pkmn: Pokemon) {
+    console.log('hi');
+    this.currentPlayerPokemon = pkmn;
+    this.currentMessage = 'Du wechselst ' + pkmn.name + ' ein!';
+    this.menuPokemon = false;
+    this.selectingAfterDefeat = false;
+    await this.delay(2000);
+    this.currentMessage = 'Du bist dran!';
   }
 
   private initPokemon() {
@@ -72,6 +111,12 @@ export class FightComponent implements OnInit {
     this.currentOpponentPokemon= this.opponentTeam[0];
   }
 
+  private async selectNewPokemon() {
+    this.playerTurn = true;
+    this.menuPokemon = true;
+    this.selectingAfterDefeat = true;
+  }
+
   private initPlayerTurn() {
     this.playerTurn = true;
   }
@@ -88,22 +133,53 @@ export class FightComponent implements OnInit {
       this.currentMessage = this.currentOpponentPokemon.name + '  (GEGNER) setzt ' + opponentMove.name + ' ein!';
       await this.delay(2000);
       await this.executeAttack(opponentMove, this.currentOpponentPokemon, this.currentPlayerPokemon);
+
+      if(this.getStat('hp', this.currentPlayerPokemon).current_stat <= 0) {
+        this.currentMessage = this.currentPlayerPokemon.name + ' wurde besiegt!';
+        this.currentPlayerPokemonDead = true;
+        return;
+      }
+
     } else if (this.isPlayerPriorityHigher()) {
       this.currentMessage = this.currentPlayerPokemon.name + ' setzt ' + attack.name + ' ein!';
       await this.delay(2000);
       await this.executeAttack(attack, this.currentPlayerPokemon, this.currentOpponentPokemon);
 
+      if(this.getStat('hp', this.currentOpponentPokemon).current_stat <= 0) {
+        this.currentMessage = this.currentOpponentPokemon.name + ' (GEGNER) wurde besiegt!';
+        this.currentOpponentPokemonDead = true;
+        return;
+      }
+
       this.currentMessage = this.currentOpponentPokemon.name + '  (GEGNER) setzt ' + opponentMove.name + ' ein!';
       await this.delay(2000);
       await this.executeAttack(opponentMove, this.currentOpponentPokemon, this.currentPlayerPokemon);
+
+      if(this.getStat('hp', this.currentPlayerPokemon).current_stat <= 0) {
+        this.currentMessage = this.currentPlayerPokemon.name + ' wurde besiegt!';
+        this.currentPlayerPokemonDead = true;
+        return;
+      }
     } else {
       this.currentMessage = this.currentOpponentPokemon.name + '  (GEGNER) setzt ' + opponentMove.name + ' ein!';
       await this.delay(2000);
       await this.executeAttack(opponentMove, this.currentOpponentPokemon, this.currentPlayerPokemon);
 
+      if(this.getStat('hp', this.currentPlayerPokemon).current_stat <= 0) {
+        this.currentMessage = this.currentPlayerPokemon.name + ' wurde besiegt!';
+        this.currentPlayerPokemonDead = true;
+        return;
+      }
+
       this.currentMessage = this.currentPlayerPokemon.name + ' setzt ' + attack.name + ' ein!';
       await this.delay(2000);
       await this.executeAttack(attack, this.currentPlayerPokemon, this.currentOpponentPokemon);
+
+      if(this.getStat('hp', this.currentOpponentPokemon).current_stat <= 0) {
+        this.currentMessage = this.currentOpponentPokemon.name + ' (GEGNER) wurde besiegt!';
+        this.currentOpponentPokemonDead = true;
+        return;
+      }
     }
   }
 
@@ -113,11 +189,11 @@ export class FightComponent implements OnInit {
 
     this.getStat('hp', target).current_stat -= Math.floor(dmgFactor * dmg);
 
-    if (this.getStat('hp', target).current_stat <= 0) {
-      this.currentMessage = target.name + ' wurde besiegt!';
-      await this.delay(2000);
+    if(this.getStat('hp', target).current_stat < 0) {
+      this.getStat('hp', target).current_stat = 0;
     }
 
+    attack.cpp--;
   }
 
   private async calcDamageMultiplicator(attack: Move, pokemon: Pokemon) {
